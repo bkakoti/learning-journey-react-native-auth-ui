18b83a726b91f2f21f6a252913a11b235249c47d
Basanta Kakoti <basantakakoti16@gmail.com>
2026-05-09 22:49:40 +0530

    basic LoginScreen setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,164 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react'
+import {
+  Image,
+  KeyboardAvoidingView, Platform,
+  Pressable,
+  StyleSheet, Text, TextInput, View
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function Index() {
+const LoginScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={{
+          flex: 1,
+          paddingHorizontal: 20,
+        }}>
+
+          <View style={{ alignItems: 'center', marginTop: 40 }}>
+            <Image
+              source={require("@/assets/images/login/circle-user-solid.png")}
+              style={{
+                width: 100,
+                height: 100,
+              }}
+            />
+          </View>
+
+          <View style={{ alignItems: 'center', margin: 20 }}>
+            <Text style={{ color: "black", fontWeight: "bold", fontSize: 26 }}>Sign IN</Text>
+            <Text style={{ color: "grey", fontWeight: "bold", fontSize: 16 }}>Let's experience the joy of telecare AI</Text>
+
+          </View>
+
+          <Text style={{ color: "black", fontWeight: "bold", fontSize: 20 }}>Email Address</Text>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              borderWidth: 1,
+              borderColor: '#ddd',
+              borderRadius: 10,
+              paddingHorizontal: 15,
+              marginBlock: 12,
+            }}
+          >
+            <Image
+              source={require("@/assets/images/login/at-solid.png")}
+              style={{
+                width: 22,
+                height: 22,
+                tintColor: 'gray',
+                marginRight: 10,
+              }}
+            />
+
+            <TextInput
+              placeholder="example@email.com"
+              style={{
+                flex: 1,
+                paddingVertical: 14,
+                fontSize: 16,
+              }}
+            />
+          </View>
+
+          <Text style={{ color: "black", fontWeight: "bold", fontSize: 20 }}>Password</Text>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              borderWidth: 1,
+              borderColor: '#ddd',
+              borderRadius: 10,
+              paddingHorizontal: 15,
+              marginBlock: 12,
+            }}
+          >
+            <Image
+              source={require("@/assets/images/login/lock-solid.png")}
+              style={{
+                width: 22,
+                height: 22,
+                tintColor: 'gray',
+                marginRight: 10,
+              }}
+            />
+
+            <TextInput
+              placeholder="password"
+              secureTextEntry
+              style={{
+                flex: 1,
+                paddingVertical: 14,
+                fontSize: 16,
+              }}
+            />
+          </View>
+
+          <View style={{ marginBlock: 20 }}>
+            <Pressable
+              style={{
+                backgroundColor: "green",
+                padding: 16,
+                borderRadius: 12,
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white", fontWeight: "bold", fontSize: 16 }}>
+                Sign In
+              </Text>
+            </Pressable>
+          </View>
+
+
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', margin: 20, gap: 20, }}>
+            <Image
+              source={require("@/assets/images/social/facebook-brands-solid.png")}
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+            <Image
+              source={require("@/assets/images/social/google-brands-solid.png")}
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+            <Image
+              source={require("@/assets/images/social/instagram-brands-solid.png")}
+              style={{
+                width: 50,
+                height: 50,
+              }}
+            />
+          </View>
+
+          <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', margin: 20 }}>
+
+            <Text style={{ color: "black", fontWeight: "bold", fontSize: 16 }}>Don't have an account yet?
+              <Text style={{ color: "green", fontWeight: "bold", fontSize: 16 }}> Sign Up</Text>
+            </Text>
+
+            <Text style={{ color: "green", fontWeight: "bold", fontSize: 16 }}>
+              Forgot your password?
+            </Text>
+          </View>
+
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default LoginScreen
+
+const styles = StyleSheet.create({})
